827d915e2711a04205c78b7a9e3cb373c7e60c0b
gacanonf <35529703+gacanonf@users.noreply.github.com>
2018-01-17 17:50:41 +0100

    Update urls.py

--- djangocon/urls.py
+++ djangocon/urls.py
@@ -3,7 +3,7 @@ Definition of urls for djangocon.
 """
 
 from datetime import datetime
-from django.conf.urls import patterns, url
+from django.conf.urls.defaults import *
 
 # Uncomment the next lines to enable the admin:
 # from django.conf.urls import include
